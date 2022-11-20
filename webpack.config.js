f81badc6f1e05cdbc822ab18a28d39ff54fb62b8
Roberto D'Alessio <info@notelseit.com>
2022-11-20 11:45:04 +0100

    varius commit

--- webpack.config.js
+++ webpack.config.js
@@ -12,129 +12,128 @@ const INTEGRATION_TESTS = path.join(__dirname, 'web/test');
 const GLOBAL_CSS = path.join(__dirname, 'web/css');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: {
-    index: ['babel-polyfill', './web/app/index'],
-    sketcher: ['babel-polyfill', './web/app/sketcher']
-  },
-  output: {
-    path: path.join(__dirname, 'dist/static'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[id].bundle.js',
-    publicPath: '/static/'
-  },
-  externals: {
-    'verb-nurbs': 'verb'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', ".ts", ".tsx"],
-    modules: [MODULES, "node_modules", WEB_APP],
-  },
-
-  devServer: {
-
-    http2: true,
-    server: {
-      type: 'https',
-      options: {
-        key: '/www/wwwroot/fabercad.italfaber.it/private.key',
-        cert: '/www/wwwroot/fabercad.italfaber.it/certificate.pem',
-
-      },
+    mode: 'development',
+    devtool: 'source-map',
+    entry: {
+        index: ['babel-polyfill', './web/app/index'],
+        sketcher: ['babel-polyfill', './web/app/sketcher']
     },
-    setupExitSignals: true,
+    output: {
+        path: path.join(__dirname, 'dist/static'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[id].bundle.js',
+        publicPath: '/static/'
+    },
+    externals: {
+        'verb-nurbs': 'verb'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', ".ts", ".tsx"],
+        modules: [MODULES, "node_modules", WEB_APP],
+    },
 
-    hot: false,
-    liveReload: false,
-    client: false,
-    static: [
-      path.join(__dirname, 'web'),
-    ],
-    setupMiddlewares(middlewares, devServer) {
-      libAssets.forEach(asset => {
-        devServer.app.get(`/lib-assets/${asset}`, function (req, res) {
-          res.sendFile(path.join(NODE_MODULES, asset))
-        })
-      });
-      return middlewares;
+    devServer: {
+        http2: true,
+        server: {
+            type: 'https',
+            options: {
+                key: '/www/wwwroot/fabercad.italfaber.it/private.key',
+                cert: '/www/wwwroot/fabercad.italfaber.it/certificate.pem',
+
+            },
+        },
+        setupExitSignals: true,
+
+        hot: false,
+        liveReload: false,
+        client: false,
+        static: [
+            path.join(__dirname, 'web'),
+        ],
+        setupMiddlewares(middlewares, devServer) {
+            libAssets.forEach(asset => {
+                devServer.app.get(`/lib-assets/${asset}`, function (req, res) {
+                    res.sendFile(path.join(NODE_MODULES, asset))
+                })
+            });
+            return middlewares;
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                loader: 'babel-loader',
+                include: [MODULES, WEB_APP, INTEGRATION_TESTS]
+            },
+            {
+                test: /\.(less|css)$/,
+                include: [GLOBAL_CSS, INTEGRATION_TESTS],
+                use: [
+                    'style-loader',
+                    'css-loader?-url',
+                    'less-loader',
+                ]
+            },
+            {
+                test: /\.(css)$/,
+                include: [NODE_MODULES],
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                oneOf: [
+                    {
+                        test: /\.(less|css)$/,
+                        include: [path.resolve(MODULES, 'ui/styles/global')],
+                        use: [
+                            'style-loader',
+                            'css-loader',
+                            'less-loader'
+                        ]
+                    },
+                    {
+                        test: /\.(less|css)$/,
+                        include: [MODULES, WEB_APP],
+                        use: [
+                            'style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: {
+                                        mode: 'local',
+                                        getLocalIdent: (context, localIdentName, localName) => generateCSSScopedName(localName, context.resourcePath),
+                                    },
+                                    url: false
+                                }
+                            },
+                            'less-loader'
+                        ]
+                    }
+                ],
+            },
+            {
+                test: /\.wasm$/,
+                type: "javascript/auto",
+                loader: "file-loader"
+            },
+            {
+                test: /\.svg$/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader'
+                    },
+                ],
+            },
+        ]
+    },
+    node: {
+        __dirname: true
     }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        loader: 'babel-loader',
-        include: [MODULES, WEB_APP, INTEGRATION_TESTS]
-      },
-      {
-        test: /\.(less|css)$/,
-        include: [GLOBAL_CSS, INTEGRATION_TESTS],
-        use: [
-          'style-loader',
-          'css-loader?-url',
-          'less-loader',
-        ]
-      },
-      {
-        test: /\.(css)$/,
-        include: [NODE_MODULES],
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        oneOf: [
-          {
-            test: /\.(less|css)$/,
-            include: [path.resolve(MODULES, 'ui/styles/global')],
-            use: [
-              'style-loader',
-              'css-loader',
-              'less-loader'
-            ]
-          },
-          {
-            test: /\.(less|css)$/,
-            include: [MODULES, WEB_APP],
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: {
-                    mode: 'local',
-                    getLocalIdent: (context, localIdentName, localName) => generateCSSScopedName(localName, context.resourcePath),
-                  },
-                  url: false
-                }
-              },
-              'less-loader'
-            ]
-          }
-        ],
-      },
-      {
-        test: /\.wasm$/,
-        type: "javascript/auto",
-        loader: "file-loader"
-      },
-      {
-        test: /\.svg$/,
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader'
-          },
-        ],
-      },
-    ]
-  },
-  node: {
-    __dirname: true
-  }
 };
